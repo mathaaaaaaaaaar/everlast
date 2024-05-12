844c0829d680a2f933da03fea8722d9c52f86dbd
Shaik Mathar Syed <shaik.mathar99@gmail.com>
2024-05-12 18:40:22 -0400

    created prod update route b/e

--- backend/Routes/product.js
+++ backend/Routes/product.js
@@ -10,6 +10,27 @@ router.post('/', async (req, res) => {
     res.send(result);
   });
 
+  router.put('/products/:name', async (req, res) => {
+    const { name } = req.params.name;
+    const { wishlistCount, purchaseCount } = req.body;
+
+    try {
+        const product = await Product.findOne(name);
+        if (!product) {
+            return res.status(404).send('Product not found');
+        }
+
+        product.wishlistCount = wishlistCount;
+        product.purchaseCount = purchaseCount;
+
+        await product.save();
+
+        res.send(product);
+    } catch (err) {
+        res.status(500).send('Server error');
+    }
+});
+
 router.get('/', async (req, res) => {
     const products = await Product.find();
     res.send(products);
